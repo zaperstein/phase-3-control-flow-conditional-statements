41f4c10f450a8a77e177e715ad107577354bad70
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-06-30 10:13:31 +0200

    commit tree fc025d0a3b14ad1d996db0130a0ba154ce839747

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,43 @@
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It\'s brisk out there!"
+  elsif temperature < 65
+    #and operator????
+    "It\'s a little chilly out there!"
+  elsif temperature > 85
+    "It\'s too dang hot out there!"
+  else 
+    "It\'s perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  f = num % 3 == 0 ? 'Fizz' : nil
+  b = num % 5 == 0 ? 'Buzz' : nil
+  f || b ? "#{ f }#{ b }" : num
+  #define variables as ternary expressions, then put them in an expression, rememvering the last line of a ruby function is automatically the return. variable interpolation used here using #
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2 
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else 
+    puts "Invalid operation!"
+  end
 end
 
